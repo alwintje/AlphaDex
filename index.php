be2823ab0234953c6d9407383b03a351dd87efa6
Jordygroote <jordygroote@live.nl>
2015-11-02 15:24:35 +0100

    front-end gemaakt

--- index.php
+++ index.php
@@ -6,6 +6,27 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	
+	<div class="container">
+		<div class="grid">
+			<div class="grid-item one-whole">
+				<div class="absolute-center">
+					<div class="login-window">
+						<div class="login-form">
+							<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+								<fieldset class="loginform-fieldset">
+									<input type='hidden' name='submitted' id='submitted' value='1'/>
+									<input type='text' name='username' id='username'  maxlength="50" placeholder="E-mail/Gebruikersnaam"/>
+									<input type='password' name='password' id='password' maxlength="50" placeholder="Wachtwoord"/>
+									<br>
+									<input id="submit-button" type='submit' name='Submit' value='Inloggen' />
+									 
+								</fieldset>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
