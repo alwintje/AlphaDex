1f9ef15124509e2af9caa77c8b41c78c21a392da
alwintje <alwinkroesen@gmail.com>
2015-11-16 13:28:07 +0100

    db and more?

--- api.php
+++ api.php
@@ -5,35 +5,38 @@
  * Date: 29-10-2015
  * Time: 10:57
  */
+require_once('Database.php');
+$db = new Database();
+$db->opendb();
 
-
-if(isset($_GET['api'])){
     if(isset($_GET['items'])){
         header('Content-Type: application/json');
-        $items = [
-            [
-                "id" => 1,
-                "name" => "Twan Withaar",
-                "image" => "http://partyflock.nl/images/user/738002_1938009o.jpg",
-            ],
-            [
-                "id" => 2,
-                "name" => "Odette Reinderink",
-                "image" => "http://images.all-free-download.com/images/wallpapers_large/frozen_heart_wallpaper_3d_models_3d_wallpaper_58.jpg",
-            ],
-            [
-                "id" => 3,
-                "name" => "Amber Voorhuis",
-                "image" => "http://www.planwallpaper.com/static/images/abstract_wallpaper_xVBXbWX.jpg",
-            ],
-            [
-                "id" => 4,
-                "name" => "Odette Reinderink",
-                "image" => "http://images.all-free-download.com/images/wallpapers_large/frozen_heart_wallpaper_3d_models_3d_wallpaper_58.jpg",
-            ],
-        ];
+        $offset = isset($_GET['offset']) ? $_GET['offset'] : "0";
+        $query = $db->doquery("SELECT * FROM {{table}} LIMIT ".$offset.",20","girls");
+        $items = [];
+        while($row = mysqli_fetch_array($query)){
+            $items[] = [
+                "id" => $row['id'],
+                "name" => $row['name'],
+                "image" => $row['image'],
+            ];
+        }
         echo json_encode($items);
     }
-}
+    if(isset($_GET['search'])){
+        header('Content-Type: application/json');
+
+        $offset = isset($_GET['offset']) ? $_GET['offset'] : "0";
+        $query = $db->doquery("SELECT * FROM {{table}} WHERE name like '%".$_GET['search']."%' LIMIT ".$offset.",20","girls");
+        $items = [];
+        while($row = mysqli_fetch_array($query)){
+            $items[] = [
+                "id" => $row['id'],
+                "name" => $row['name'],
+                "image" => $row['image'],
+            ];
+        }
+        echo json_encode($items);
+    }
 ?>
 
