b04431fc84cc8488a7e2e498f5730c4a4cc47f47
alwintje <alwinkroesen@gmail.com>
2015-11-30 15:16:18 +0100

    API

--- api.php
+++ api.php
@@ -12,7 +12,7 @@ $db->opendb();
     if(isset($_GET['items'])){
         header('Content-Type: application/json');
         $offset = isset($_GET['offset']) ? $_GET['offset'] : "0";
-        $query = $db->doquery("SELECT * FROM {{table}} LIMIT ".$offset.",20","girls");
+        $query = $db->doquery("SELECT id,name,image FROM {{table}} LIMIT ".$offset.",20","girls");
         $items = [];
         while($row = mysqli_fetch_array($query)){
             $items[] = [
@@ -38,7 +38,7 @@ $db->opendb();
         header('Content-Type: application/json');
 
         $offset = isset($_GET['offset']) ? $_GET['offset'] : "0";
-        $query = $db->doquery("SELECT * FROM {{table}} WHERE name like '%".$_GET['search']."%' LIMIT ".$offset.",20","girls");
+        $query = $db->doquery("SELECT id,name,image FROM {{table}} WHERE name like '%".$_GET['search']."%' LIMIT ".$offset.",20","girls");
         $items = [];
         while($row = mysqli_fetch_array($query)){
             $items[] = [
@@ -48,6 +48,51 @@ $db->opendb();
             ];
         }
         echo json_encode($items);
+    }
+    if(isset($_GET['id'])){
+        header('Content-Type: application/json');
+
+        $query = $db->doquery("SELECT * FROM {{table}} WHERE id='".$_GET['id']."' ","girls");
+        $row = mysqli_fetch_array($query);
+
+        $item = [
+            "id" => $row['id'],
+            "name" => $row['name'],
+            "image" => $row['image'],
+            "hair_color" => $row['hair_color'],
+            "eye_color" => $row['eye_color'],
+            "birth" => $row['birthday'],
+            "tier" => $row['tier'],
+            "rating" => $row['rating'],
+            "rates" => $row['rates'],
+        ];
+        echo json_encode($item);
+    }
+    if(isset($_GET['plus'])){
+        header('Content-Type: application/json');
+
+        $query = $db->doquery("SELECT * FROM {{table}} WHERE id='".$_GET['plus']."' ","girls");
+        $row = mysqli_fetch_array($query);
+        $rating = intval($row['rating']);
+        $rates = intval($row['rates']);
+        $rating++;
+        $rates++;
+
+        $db->doquery("UPDATE {{table}} SET rating='".$rating."',rates='".$rates."' WHERE id='".$_GET['plus']."'","girls");
+        echo json_encode(true);
+    }
+    if(isset($_GET['minus'])){
+        header('Content-Type: application/json');
+
+        $query = $db->doquery("SELECT * FROM {{table}} WHERE id='".$_GET['minus']."' ","girls");
+        $row = mysqli_fetch_array($query);
+        $rating = intval($row['rating']);
+        $rates = intval($row['rates']);
+        $rating--;
+        $rates++;
+        $db->doquery("UPDATE {{table}} SET rating='".$rating."',rates='".$rates."' WHERE id='".$_GET['minus']."'","girls");
+
+        echo json_encode(true);
     }
 ?>
 
